7c62f5d7585a8f66b4a9fad07a6c6348cf11ec3c
Look-Its-Sky <joubertjude@gmail.com>
2021-12-01 21:57:27 -0600

    Update

--- tumultbts/core/src/com/jude/tumultbts/Keyboard.java
+++ tumultbts/core/src/com/jude/tumultbts/Keyboard.java
@@ -2,8 +2,6 @@ package com.jude.tumultbts;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import java.util.Timer;
-import java.util.TimerTask;
 
 /*
 NOTE:
@@ -19,122 +17,23 @@ public class Keyboard extends Thread
 	private String currentInput;
 	private String mode;
 
-	private int writer;
+	//Constants
+	private final int UP = Input.Keys.W;
+	private final int LEFT = Input.Keys.A;
+	private final int DOWN = Input.Keys.S;
+	private final int RIGHT = Input.Keys.D;
+	private final int j = Input.Keys.J;
+	private final int k = Input.Keys.K;
 
-	private Timer timer;
+	private final String nlight = String.valueOf(j);
+	private final String slightl = String.valueOf(LEFT + " " + j);
+	private final String slightr = String.valueOf(RIGHT + " " + j);
+	private final String dlight = String.valueOf(DOWN + " " + j);
 
-	//Attacks and key stuff
-	//Represented as ASCII int
-
-	/*
-	NOTE: THESE ENUMS ARE A WORK IN PROGRESS AND ARE SUBJECT TO CHANGE
-	THERE ARE A SNIPPET FROM A KEYBOARD CLASS I AM WRITING
-	TO MAKE THIS STUFF EASIER
-	IT IS MESSY AND WILL BE FIXED
-	 */
-	public enum keys
-	{
-		LEFT(Input.Keys.A),
-		RIGHT(Input.Keys.D),
-		UP(Input.Keys.W),
-		DOWN(Input.Keys.S),
-		SPACE(Input.Keys.SPACE),
-		j(Input.Keys.J),
-		k(Input.Keys.K);
-
-
-		private final int label;
-
-		private keys(int label)
-		{
-			this.label = label;
-		}
-
-		//This should be the same concept as toString() in atk enum
-		public int toInt()
-		{
-			switch(this)
-			{
-				case LEFT:
-					return Input.Keys.A;
-
-				case RIGHT:
-					return Input.Keys.D;
-
-				case UP:
-					return Input.Keys.W;
-
-				case DOWN:
-					return Input.Keys.S;
-
-				default:
-					return 999;
-			}
-		}
-
-	}
-
-
-
-
-	//Translates ASCII ints into actual attacks in the form of Strings
-	public enum atk
-	{
-		nlight(String.valueOf(keys.j)),
-		slightl(String.valueOf(keys.LEFT) + " " + String.valueOf(keys.j)),
-		slightr(String.valueOf(keys.LEFT) + " " + String.valueOf(keys.j)),
-		dlight(String.valueOf(keys.DOWN) + " " + String.valueOf(keys.j)),
-
-		nheavy(String.valueOf(keys.k)),
-		sheavyl(String.valueOf(keys.LEFT) + " " + String.valueOf(keys.k)),
-		sheavyr(String.valueOf(keys.LEFT) + " " + String.valueOf(keys.k)),
-		dheavy(String.valueOf(keys.DOWN) + " " + String.valueOf(keys.k));
-
-		private final String label;
-
-
-		private atk(String label)
-		{
-			this.label = label;
-		}
-
-
-		//Checks what enum itself is and returns a string to accurately represent itself
-		public String toString()
-		{
-
-			switch(this)
-			{
-				case nlight:
-					return String.valueOf(keys.j);
-
-				case slightl:
-
-				case slightr:
-					System.out.println("slight");
-					return String.valueOf(keys.LEFT) + " " + String.valueOf(keys.j);
-
-				case dlight:
-					return String.valueOf(keys.DOWN) + " " + String.valueOf(keys.j);
-
-				case nheavy:
-					return String.valueOf(keys.k);
-
-				case sheavyl:
-
-				case sheavyr:
-					return String.valueOf(keys.LEFT) + " " + String.valueOf(keys.k);
-
-				case dheavy:
-					return String.valueOf(keys.DOWN) + " " + String.valueOf(keys.k);
-
-				default:
-					return "";
-
-			}
-		}
-	}
-
+	private final String nheavy = String.valueOf(k);
+	private final String sheavyl = String.valueOf(LEFT + " " + k);
+	private final String sheavyr = String.valueOf(RIGHT + " " + k);
+ 	private final String dheavy = String.valueOf(DOWN + " " + k);
 
 	public void toggleRPG()
 	{
@@ -146,44 +45,10 @@ public class Keyboard extends Thread
 		mode = "fighting";
 	}
 
-	//Misc stuff also represented as strings
-	public enum misc
-	{
-		jump(String.valueOf(keys.SPACE));
-
-		private final String label;
-
-		private misc(String label)
-		{
-			this.label = label;
-		}
-	}
-
-
-
 	public Keyboard()
 	{
 		currentKeys = new int[1];
 		currentInput = "";
-		writer = 0;
-		timer = new Timer();
-	}
-
-	//For fighting once it is implemented
-	private void handleInput()
-	{
-		if(writer + 1 > 1) writer = 0;
-
-		if(Gdx.input.isKeyPressed(keys.LEFT.toInt()))
-		{
-			currentKeys[writer] = keys.LEFT.toInt();
-			writer++;
-		}
-
-		else if(Gdx.input.isKeyPressed(keys.RIGHT.toInt()))
-		{
-			currentKeys[writer] = keys.RIGHT.toInt();
-		}
 	}
 
 	public String returnCurrentInput()
@@ -196,7 +61,8 @@ public class Keyboard extends Thread
 	{
 		if (mode == "fight")
 		{
-			handleInput();
+			//Handle fighting inputs and combos
+            //TODO: inputs/combos
 		}
 
 		if (mode == "rpg")
@@ -223,12 +89,9 @@ public class Keyboard extends Thread
 		}
 	}
 
-	//There is a delay and it does not work; I am still troubleshooting
+
 	public void run()
 	{
-		while(true)
-		{
-			checkForInput();
-		}
+		checkForInput();
 	}
 }
